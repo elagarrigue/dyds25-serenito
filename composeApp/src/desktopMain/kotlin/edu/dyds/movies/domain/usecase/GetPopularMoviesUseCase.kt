b5c82cb3a365742f7f887c0178c3a89f7f60613c
josefina bacci <josefinabacci015@gmail.com>
2025-05-21 16:42:54 -0300

    Clases añadidas para futura implementación

--- composeApp/src/desktopMain/kotlin/edu/dyds/movies/domain/usecase/GetPopularMoviesUseCase.kt
+++ composeApp/src/desktopMain/kotlin/edu/dyds/movies/domain/usecase/GetPopularMoviesUseCase.kt
@@ -1,2 +1,3 @@
 package edu.dyds.movies.domain.usecase
 
+//Intermediarios entre los ViewModel y Repository, este en particular se encarga de obtener peliculas del repositorio
--- composeApp/src/desktopMain/kotlin/edu/dyds/movies/presentation/home/HomeViewModel.kt
+++ composeApp/src/desktopMain/kotlin/edu/dyds/movies/presentation/home/HomeViewModel.kt
@@ -1,2 +1,4 @@
 package edu.dyds.movies.presentation.home
 
+//Como tenemos MoviesViewModel, que se encarga de manejar la lógica de la pantalla principal (lista de peliculas) y de la pantalla para los detalles
+//Lo dividimos en dos para cumplir con el principio S de SOLID, donde tenemos entonces por separado el manejo de esas dos pantallas
