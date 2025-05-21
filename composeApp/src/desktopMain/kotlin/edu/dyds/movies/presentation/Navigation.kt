d6c2f913338bcff40d1df615cc170ca811b5130b
MiliVives <milivives@gmail.com>
2025-05-21 16:25:42 -0300

    Reorganizado

--- composeApp/src/desktopMain/kotlin/edu/dyds/movies/presentation/Navigation.kt
+++ composeApp/src/desktopMain/kotlin/edu/dyds/movies/presentation/Navigation.kt
@@ -1,6 +1,6 @@
 @file:Suppress("FunctionName")
 
-package edu.dyds.movies
+package edu.dyds.movies.presentation
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavGraphBuilder
@@ -10,7 +10,9 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
-import edu.dyds.movies.MoviesDependencyInjector.getMoviesViewModel
+import edu.dyds.movies.di.MoviesDependencyInjector.getMoviesViewModel
+import edu.dyds.movies.presentation.detail.DetailScreen
+import edu.dyds.movies.presentation.home.HomeScreen
 
 private const val HOME = "home"
 
@@ -47,7 +49,10 @@ private fun NavGraphBuilder.detailDestination(navController: NavHostController) 
         val movieId = backstackEntry.arguments?.getInt(MOVIE_ID)
 
         movieId?.let {
-            DetailScreen(getMoviesViewModel(), it, onBack = { navController.popBackStack() })
+            DetailScreen(
+                getMoviesViewModel(),
+                it,
+                onBack = { navController.popBackStack() })
         }
     }
 }
