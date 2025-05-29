2fb00e346f084cab76cd09e0b0c9a68473954faf
josefina bacci <josefinabacci015@gmail.com>
2025-05-29 17:25:18 -0300

    Solucion imagen peliculas malas

--- composeApp/src/desktopMain/kotlin/edu/dyds/movies/presentation/home/HomeScreen.kt
+++ composeApp/src/desktopMain/kotlin/edu/dyds/movies/presentation/home/HomeScreen.kt
@@ -31,6 +31,9 @@ import dydsproject.composeapp.generated.resources.app_name
 import dydsproject.composeapp.generated.resources.error
 import edu.dyds.movies.domain.entity.QualifiedMovie
 import org.jetbrains.compose.resources.stringResource
+import androidx.compose.foundation.Image
+import androidx.compose.ui.res.painterResource
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -139,8 +142,8 @@ private fun BadMovieItem(movie: Movie) {
         onCloseRequest = { dialogState = false },
         visible = dialogState
     ) {
-        AsyncImage(
-            model = this::class.java.getResource("/images/too_bad.png")?.toString(),
+        Image(
+            painter = painterResource("images/too_bad.png"),
             contentDescription = null,
             contentScale = ContentScale.Fit,
             modifier = Modifier
